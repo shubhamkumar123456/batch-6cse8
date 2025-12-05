ffdadb3d16d21b4f4150ac9f71b71118b948e54f
shubham kumar <clboy768@gmail.com>
2025-12-05 15:21:31 +0530

    first commit

---
+++ dummy.js
@@ -0,0 +1,83 @@
+// Variable  --> are used to store the data
+
+// let x = 10;
+// let x = 5  // redecleration;
+
+
+// console.log(x)// redecleration is not possible
+
+
+// let x = 10;
+//     x = 6  //reassigning;
+//     console.log(x)  //reassign is possible
+
+
+// var x = 10;
+// var x = 6;  //redclearation
+// console.log(x)
+
+
+// var x = 10;
+//     x = 8;
+//     console.log(x)
+
+
+// const x = 10;
+// const x = 9;
+// console.log(x)  //can not redeclare in case of const
+
+
+// const x = 10;
+//     x = 2;
+//     console.log(x)
+
+
+// DataType  -->primitive and non primitive
+// 1)Primitive -->Datatype that can store only single value example  
+//a) number
+//b) String
+//c) boolean
+//d)undefined --> default value used by js when variable is declared but value is not assigned it gives undefined
+//e)Null --> user created value that is equal to nothing
+
+// let z ;
+// console.log(z)   give undefined
+
+
+// Rules of variable decleration --> 
+// 1)Variable name should not start with number or special characters except $ _
+
+
+// operators  --> 
+// 1)Mathmetical operator -->  + , - , /, % (modulous),
+
+// let x = 10;
+// let y = 5;
+
+// console.log(x+y)   // 15
+// console.log(x-y)   // 5
+// console.log(x/y)  // 2
+// console.log(x%y)   //0 
+
+// 2)Comparision operator -->  > , <, ==, === ,!= , >= , <=
+
+// == check or compare only value
+// === check both data type and value
+
+// conditional operator
+// a) if
+// b) if else
+// c) if else if... else
+// d)nested if
+// e)ladder if
+// f) switch case
+
+
+
+
+
+console.log(10);   // 10
+
+console.log(x)   //
+
+
